9680c8e6e34d5caeb109bf87c91b15c6825471f5
Rolando Urquiza <rolurquiza@gmail.com>
2017-01-18 17:43:20 -0500

    Use class instead of React function

--- src/js/components/webrosie.jsx
+++ src/js/components/webrosie.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default React.createClass({
-  render: function () {
+export default class WebRosie extends React.Component {
+  render () {
     return (
       <div>
         Hey! This is just an auto-generated component.
@@ -9,4 +9,4 @@ export default React.createClass({
       </div>
     );
   }
-});
+};
