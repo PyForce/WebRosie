6a9f90d633b3de5de222f79ea7f4f545e309e21a
Rolando Urquiza <rolurquiza@gmail.com>
2017-01-18 10:39:07 -0500

    Add files from flask-rect template

---
+++ test/components/webrosie-test.jsx
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+
+import React from 'react';
+import ReactShallowRenderer from 'react-addons-test-utils';
+
+import WebRosie from '../../src/js/components/webrosie.jsx';
+
+describe('WebRosie component', function () {
+  before(() => {
+    let shallowRenderer = ReactShallowRenderer.createRenderer();
+    shallowRenderer.render(
+      <Flaskreact />
+    );
+    this.result = shallowRenderer.getRenderOutput();
+  });
+
+  it('renders a <div>', () => {
+    expect(this.result.type).to.equal('div');
+  });
+});
