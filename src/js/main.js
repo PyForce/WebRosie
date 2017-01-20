07a501f7a24e49c2ded6e2ebbaa3507a4f89007c
Rolando Urquiza <rolurquiza@gmail.com>
2017-01-20 11:21:00 -0500

    Add main app bar

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,25 @@ import $ from 'jquery'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import WebRosie from './components/webrosie'
 import store from './store'
 
 
 function init () {
-  let app = $('[data-section="app"]').get(0);
+  let app = $('#app').get(0);
 
   render(
     <Provider store={store}>
-      <WebRosie />
+      <MuiThemeProvider>
+        <WebRosie />
+      </MuiThemeProvider>
     </Provider>,
     app
   );
