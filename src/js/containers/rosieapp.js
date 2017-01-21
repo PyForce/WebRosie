a23b69185f7815549ee4aec5c76cabf98adf57bb
Rolando Urquiza <rolurquiza@gmail.com>
2017-01-21 18:49:27 -0500

    Fix redux container for main app

---
+++ src/js/containers/rosieapp.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux'
+
+import MainApp from '../components/mainapp'
+import { addRobot } from '../actions'
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onAddRobot: (...params) => {
+            dispatch(addRobot(...params));
+        }
+    }
+}
+
+const RosieApp = connect(
+    null,
+    mapDispatchToProps
+)(MainApp)
+
+export default RosieApp;
