f1cbd14295b863908d9c7b4f0b49773c705ddbba
Rolando Urquiza <rolurquiza@gmail.com>
2017-01-21 03:18:14 -0500

    Add custom theme for rosie

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,18 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { blue500 } from 'material-ui/styles/colors'
 
 import WebRosie from './components/webrosie'
 import store from './store'
+
+
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: blue500
+    }
+});
 
 
 function init () {
@@ -18,7 +27,7 @@ function init () {
 
   render(
     <Provider store={store}>
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <WebRosie />
       </MuiThemeProvider>
     </Provider>,
