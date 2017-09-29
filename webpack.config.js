b486dcce864ad58abe4a74ab9867c844612f5a37
Rolando Urquiza <rolurquiza@gmail.com>
2017-09-28 20:48:39 -0400

    Fix webpack for updated version

--- webpack.config.js
+++ webpack.config.js
@@ -10,34 +10,30 @@ config.entry = [
 ];
 
 config.module = {
-  loaders: [{
-    loader: 'babel-loader',
+  rules: [{
     test: /\.jsx?$/,
+    loader: 'babel-loader',
     exclude: /node_modules/
   }],
-  postLoaders: []
 };
 
 config.output = {
   filename: '[name].bundle.js',
   chunkFilename: '[id].chunk.js',
   path: path.join(__dirname, 'public', 'static', 'js'),
-  publicPath: '/static/js',
-  devtoolModuleFilenameTemplate: '[resourcePath]',
-  devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  publicPath: '/static/js'
 };
 
 config.plugins = [];
 
 config.resolve = {
   extensions: [
-    '',
     '.js',
     '.jsx'
   ],
-  modulesDirectories: [
-    './src/js',
-    './node_modules/'
+  modules: [
+    path.join(__dirname, 'src'),
+    path.join(__dirname, 'node_modules')
   ]
 };
 
